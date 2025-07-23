364175be1018c06a83885a55cc142684cf56fda6
davidelatina <davide.latina616@gmail.com>
2025-07-23 16:09:34 +0200

    Create AutoreDTO.java

---
+++ libreria/src/main/java/com/example/todolist/dto/AutoreDTO.java
@@ -0,0 +1,19 @@
+package com.example.todolist.dto;
+
+import java.util.List;
+import com.example.todolist.model.Libro;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AutoreDTO {
+    private Long id;
+    private String nome;
+    private List<Libro> libri;
+}
+
