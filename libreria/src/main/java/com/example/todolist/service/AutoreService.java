c4373ce3317ff5ae2229b59429852ea74283aba4
davidelatina <davide.latina616@gmail.com>
2025-07-23 17:46:45 +0200

    Update AutoreService.java

--- libreria/src/main/java/com/example/todolist/service/AutoreService.java
+++ libreria/src/main/java/com/example/todolist/service/AutoreService.java
@@ -1,5 +1,7 @@
 package com.example.todolist.service;
 
+import com.example.todolist.dto.AutoreDTO;
+import com.example.todolist.mapper.AutoreMapper;
 import com.example.todolist.model.Autore;
 import com.example.todolist.repository.AutoreRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,22 +9,32 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
 @Service
 @RequiredArgsConstructor
-public class AutoreService
-{
+public class AutoreService {
     private final AutoreRepository autoreRepository;
 
-    public List<Autore> findAll() {
-        return autoreRepository.findAll();
+    public List<AutoreDTO> findAll() {
+        return autoreRepository.findAll().stream().map(AutoreMapper::toDTO).toList();
     }
 
-    public Autore findById(Long id) {
-        return autoreRepository.findById(id).orElseThrow(() -> new RuntimeException("Autore non trovato"));
+    public AutoreDTO findById(Long id) {
+        return AutoreMapper.toDTO(autoreRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Autore non trovato")));
     }
 
-    public Autore save(Autore autore) {
-        return autoreRepository.save(autore);
+    public AutoreDTO create(AutoreDTO dto) {
+        return AutoreMapper.toDTO(autoreRepository.save(AutoreMapper.toEntity(dto)));
+    }
+
+    public AutoreDTO update(AutoreDTO dto) {
+        Autore autore = autoreRepository.findById(dto.getId())
+                .orElseThrow(() -> new RuntimeException("Autore non trovato."));
+
+        autore.setNome(dto.getNome());
+        
+        return AutoreMapper.toDTO(autoreRepository.save(autore));
     }
 
     public void delete(Long id) {
