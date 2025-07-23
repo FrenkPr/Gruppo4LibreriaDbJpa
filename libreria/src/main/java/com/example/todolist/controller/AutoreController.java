ee0f96d1eb38e2b9e3862e01456f91c7c01a3161
davidelatina <davide.latina616@gmail.com>
2025-07-23 17:26:01 +0200

    Update AutoreController.java

--- libreria/src/main/java/com/example/todolist/controller/AutoreController.java
+++ libreria/src/main/java/com/example/todolist/controller/AutoreController.java
@@ -1,9 +1,11 @@
 package com.example.todolist.controller;
 
+import com.example.todolist.dto.AutoreDTO;
 import com.example.todolist.model.Autore;
 import com.example.todolist.service.AutoreService;
 import com.example.todolist.service.LibroService;
 import com.example.todolist.model.Libro;
+import com.example.todolist.model.Utente;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,24 +15,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/autore")
 @RequiredArgsConstructor
-public class AutoreController
-{
+public class AutoreController {
     private final AutoreService autoreService;
     private final LibroService libroService;
 
+
+
     @GetMapping
-    public List<Autore> getAllAutori() {
-        return autoreService.findAll();
+    public ResponseEntity<List<AutoreDTO>> getAllAutori() {
+        return ResponseEntity.ok(autoreService.findAll());
     }
 
     @GetMapping("/{id}")
-    public Autore getAutoreById(@PathVariable Long id) {
-        return autoreService.findById(id);
+    public ResponseEntity<AutoreDTO> getAutoreById(@PathVariable Long id) {
+        return ResponseEntity.ok(autoreService.findById(id));
     }
 
     @PostMapping
-    public Autore createAutore(@RequestBody Autore autore) {
-        return autoreService.save(autore);
+    public ResponseEntity<AutoreDTO> createAutore(@RequestBody Autore autore) {
+        return ResponseEntity.ok(autoreService.create(autore));
     }
 
     @DeleteMapping("/{id}")
@@ -39,9 +42,9 @@ public class AutoreController
         return ResponseEntity.ok("Autore eliminato");
     }
 
-    
+
     @GetMapping("/{id}/libro")
-    public List<Libro> getLibroByAutore(@PathVariable Long id) {
-        return libroService.findByAutoreId(id);
+    public ResponseEntity<List<Libro>> getLibroByAutore(@PathVariable Long id) {
+        return ResponseEntity.ok(libroService.findByAutoreId(id));
     }
 }
